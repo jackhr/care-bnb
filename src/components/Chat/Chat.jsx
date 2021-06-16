05488d5f460c96562e79ccaeaddde8020e813992
jackhr <jc2o@mac.com>
2021-06-16 17:13:32 -0400

    final commit

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -2,7 +2,6 @@ import React,{ useState, useEffect }  from 'react';
 import { Container, Col, Row } from "react-bootstrap";
 import io from "socket.io-client";
 import * as userService from "../../utilities/users-service";
-import './Chat.css'
 
 const socket = io();
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -20,7 +20,7 @@ export default function NavBar({ user, setUser }) {
           <Link to="/caregivers"><img className="nav-icons" src="media/main-page.svg" alt="icon of a house to main page." /></Link>
           <Link to="/chat"><img className="nav-icons" src="media/chat-page.svg" alt="icon of chat bubble to chat room page." /></Link>
           {/* ^^^^^^^^^^^^ This will be the link to the chat feature */}
-          {/* <Link to="" onClick={handleLogOut}>Log Out</Link> */}
+          <Link to="" onClick={handleLogOut}>Log Out</Link>
         </>
       )}
 
