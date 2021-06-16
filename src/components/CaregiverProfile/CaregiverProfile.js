5ae9f8623c02563fa02def927634515c153da62d
jackhr <jc2o@mac.com>
2021-06-16 16:40:12 -0400

    style index page

--- src/components/CaregiverProfile/CaregiverProfile.js
+++ src/components/CaregiverProfile/CaregiverProfile.js
@@ -1,4 +1,6 @@
 import React from "react";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 
 export default function caregiverProfile({
   fname,
@@ -33,22 +35,14 @@ profile_image: ''
 {
   return (
     <div className="profile-container">
-      <h1>
-        Name: {fname} {lname}
-      </h1>
-
       <img src={profile_image} alt="" />
-      <h2>Email: {email}</h2>
-      <h2>Phone: {phone_number}</h2>
-      <h2>Location: {location}</h2>
-      <h2>Age: {age}</h2>
-      <h2>About Me: {about}</h2>
-      <h2>Best Time To Be Reached: {best_time}</h2>
-      <h2>Certifications: {credentials}</h2>
-      <h2>Rate: {rate}</h2>
-      <h2>LinkedIn: {linkedin}</h2>
-      <h2>Facebook: {facebook}</h2>
-      <h2>Instagram: {instagram}</h2>
+      <div className="name-container">
+        <div>
+          <h1>{fname} {lname}, {age}</h1>
+          <CheckCircleIcon style={{marginLeft: "2vmin",fontSize: "5vmin", color: "#588B8B" }} fontSize="large" />
+        </div>
+        <FavoriteBorderIcon style={{ fontSize: "5vmin" }} />
+      </div>
     </div>
   );
 }
